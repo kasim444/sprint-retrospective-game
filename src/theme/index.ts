c3cd8b9c3a4b0143a3ff2fb94c4855b7fabca912
Kasim <kasim1411@gmail.com>
2023-01-19 21:55:47 +0300

    test google adsense integration

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -17,6 +17,32 @@ const theme = extendTheme({
         minHeight: "full",
         overflowX: "hidden",
       },
+      "#root": {
+        display: "grid",
+        gridTemplateAreas: '"header" "main" "left-aside" "right-aside"',
+        gridTemplateColumns: "1fr",
+      },
+      header: {
+        gridArea: "header",
+      },
+      main: {
+        gridArea: "main",
+      },
+      aside: {
+        "&:first-of-type": {
+          gridArea: "left-aside",
+        },
+        "&:last-of-type": {
+          gridArea: "right-aside",
+        },
+      },
+      "@media screen and (min-width: 1200px)": {
+        "#root": {
+          gridTemplateAreas:
+            '"header header header" "left-aside main right-aside"',
+          gridTemplateColumns: "336px 1fr 336px",
+        },
+      },
     },
   },
 });
